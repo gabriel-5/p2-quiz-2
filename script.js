ba7ece44652542b83195d537e85fe84b355759f1
Gabriel M <gabe.manzi@gmail.com>
2021-09-20 16:24:05 +0100

    quiz 2 begin

---
+++ script.js
@@ -0,0 +1,18 @@
+let qNum = 0;
+let aNum = 0;
+
+let scoreCounts = {
+    wilfredLight: 0,
+    basqueCrize: 0,
+    ozzyRanvalge: 0,
+    jueddsKross: 0,
+    miria: 0
+}
+
+let answers = ["Merchant", "Soldier", "Gravekeeper", "Priest", "Fairy", "I can't see a thing", "Sharp",
+    "I wear reading glasses", "Blessed", "Magically Enhanced", "Important Key", "Plate Armour", "Shovel",
+    "Gold Cross", "Magic Potion", "Spring", "Autumn", "Summer", "Winter", "I don't mind", "Very old",
+    "I don't know yet", "Normal", "Average", "Young"]
+
+let questions = ["Which job would you prefer?", "How is your eyesight?", "What's your favourite thing?", "Season?", "How old are you?"]
+
